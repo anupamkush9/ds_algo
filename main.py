b2b3b2bb520862df8428ece65946145ea0c9f384
raviK <raviK@techatlantis.com>
2022-11-06 22:15:50 +0530

    sum of n natural no by recursion

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def print_num(n):
-    if n==0:
-        return 1
+def sum_of_num(n):
+    if n == 1:
+        return n
     else:
-        # print(n)        # for printing number from 1 to 5
-        print_num(n-1)
-        print(n)        # for printing number from 5 to 1
-print_num(5)
+        return n + sum_of_num(n-1)
+
+print(sum_of_num(5))
