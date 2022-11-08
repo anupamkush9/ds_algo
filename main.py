55785340528cc4bf6829a622e9566d91d2b56fe2
raviK <raviK@techatlantis.com>
2022-11-08 06:33:36 +0530

    length of string by recursion

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 
-def palindrom(s):
+def length_by_recursion(s):
     if len(s) == 1:
-        return s
-    return s[-1] + palindrom(s[:-1])
+        return 1
+    return 1 + length_by_recursion(s[:-1])
 
 s = input()
-if s == palindrom(s):
-    print("Palindrome No")
-else:
-    print("Not Palindrome No")
-
-
-
-
-
-
+print(length_by_recursion(s))
 
