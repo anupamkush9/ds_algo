c93381710bf650d25afe83612e2b1e401f14db5f
raviK <raviK@techatlantis.com>
2022-11-10 06:10:09 +0530

    sum_of_digits_by_recursion

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+#sum of digits of number
+def sum_of_digits(num):
+    if len(str(num)) == 2:
+        return num//10 + num%10
+    return num%10 + sum_of_digits(num//10)
 
-def length_by_recursion(s):
-    if len(s) == 1:
-        return 1
-    return 1 + length_by_recursion(s[:-1])
-
-s = input()
-print(length_by_recursion(s))
+s = int(input())
+print(sum_of_digits(s))
 
