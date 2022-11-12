b6008a8b7efae2780abc9b3214bbd60f105a4b5c
raviK <raviK@techatlantis.com>
2022-11-12 09:18:10 +0530

    captalize_first_letter_of_word_in_a_string_by_recursion

--- main.py
+++ main.py
@@ -1,50 +1,14 @@
-# program for Decimal to Binary conversion
+# Write a recursive function called capitalizeFirst. Given an array of strings, capitalize the first letter of each string in the array.
+# Example
+# capitalizeFirst(['car', 'taco', 'banana']) # ['Car','Taco','Banana']
 
-# Question 1
-def sumofDigits(n):
-    assert n>=0 and int(n) == n , 'The number has to be a postive integer only!'
-    if n == 0:
-        return 0
+def capitalizeFirst(arr):
+    result_list = []
+    if len(arr)==1:
+        result_list.append(arr[0].capitalize())
+        return result_list
     else:
-        return int(n%10) + sumofDigits(int(n/10))
+        result_list.append(arr[0].capitalize())
+        return result_list + capitalizeFirst(arr[1:])
 
-print(sumofDigits(11111))
-
-
-#Question 2
-
-def power(base,exp):
-    if exp == 0:
-        return 1
-    if(exp==1):
-        return(base)
-    if(exp!=1):
-        return (base*power(base,exp-1))
-
-print(power(4,2))
-
-# Question 3
-
-
-def gcd(a, b):
-    assert int(a) == a and int(b) == b, 'The numbers must be integer only!'
-    if a < 0:
-        a = -1 * a
-    if b < 0:
-        b = -1 * b
-    if b == 0:
-        return a
-    else:
-        return gcd(b, a%b)
-
-print(gcd(12,1.2))
-
-# Question 4
-def decimalToBinary(n):
-    if n == 0:
-        return 0
-    else:
-        return n%2 + 10*decimalToBinary(int(n/2))
-
-
-print(decimalToBinary(1))
+print(capitalizeFirst(['car', 'taco', 'banana']))
