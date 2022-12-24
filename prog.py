e7627f07859b89f67c89235dc93e8e917b4ee795
Anupamkush9 <anupam.kush9@gmail.com>
2022-12-24 12:15:57 +0530

    insert operation in array

--- prog.py
+++ prog.py
@@ -1,11 +1,14 @@
-class Solution:
-    def isPowerOfFour(self, n: int) -> bool:
-        if (n==1):
-            return True
-        if (n==0):
-            return False
-        if (n%4!=0):
-            return False
-        return self.isPowerOfFour(n//4)
+from array import *
 
-print(Solution().isPowerOfFour(5))
+arr = array('i', [1, 2, 3, 4, 5])
+
+# inserting an element at the end of the array (0)1
+arr.insert(len(arr),100)
+
+# inserting an element at the start of the array (0)n
+arr.insert(0,0)
+
+# inserting an element at the middle of the array (0)n
+arr.insert(len(arr)//2,55)
+
+print("arr:::", *arr)
