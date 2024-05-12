7e870841f028be1d5f5b3a1319ba2faa91b4b002
Anupamkush9 <anupam.kush9@gmail.com>
2024-05-12 11:38:47 +0530

    Linked list creation and displaying done

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
-def fibonacci(a, b, n):
-    if n==1:
-        return a + b
-    print(a + b)
-    return fibonacci(b, a+b, n-1)
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
 
-print(0)
-print(1)
-print(fibonacci(0,1,5))
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def insert(self, data, next=None):
+        new_node = Node(data, next)
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node  
+        
+    def printll(self):
+        if self.head is None:
+            print("Linked List is empty")
+        else:
+            current = self.head
+            while current:
+                print(f"{current.data}-->", end=" ")
+                current = current.next
+            print("None")
+
+ll = LinkedList()
+ll.insert(10)
+ll.insert(20)
+ll.insert(20)
+ll.insert(20)
+ll.insert(30)
+ll.printll()
+
