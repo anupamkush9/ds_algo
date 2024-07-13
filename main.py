653055a117dfa2239885d6de3eae26d89459832d
Anupamkush9 <anupam.kush9@gmail.com>
2024-07-13 23:43:13 +0530

    first occurance of number by recursion

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-def check_sorted_array(l):
-    if len(l) == 1:
-        return True
-    if l[0] < l[1]:
-        return check_sorted_array(l[1:])
+
+
+
+l = [100, 200, 300, 400, 5880]
+
+def first_occurance_of_number(index_, list_, target):
+    if list_[index_] == target:
+        return index_
     else:
-        return False
+        if index_ < len(l)-1:
+            index_ += 1
+            return first_occurance_of_number(index_,list_, target)
+        else:
+            return None
 
-l = [10, 20, 300, 40, 50]
-print(check_sorted_array(l))
-
-
+print(first_occurance_of_number(index_ = 0, list_=l, target=300))
