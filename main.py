92e7105861eb5b05449ce6f40646c592d2ef145d
Anupamkush9 <anupam.kush9@gmail.com>
2024-07-14 18:14:15 +0530

    find_the_length_of_string_by_recursion

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
 # Question1:
-#     For a given integer array of sizeN.You have to find all the occurrences (indices) of a given element (Key)
-#     and print them. Use a recursive function to solve this problem.
-#     Sample Input: arr[ ] = {3, 2, 4, 5, 6, 2, 7, 2, 2}, key = 2
-#     Sample Output: 1 5 7 8
+# Find the length of string
 
-
-def element_indexes(l, indexes, target, results):
-    if indexes == len(l) -1:
-        if l[indexes] == target:
-            results.append(indexes)
-            return results
-    if indexes < len(l)-1:
-        if l[indexes] == target:
-            results.append(indexes) 
-            idx = indexes+1
-            return element_indexes(l, idx, target, results)
-        idx = indexes+1
-        return element_indexes(l, idx, target, results)
-
-print(element_indexes([3, 2, 4, 5, 6, 2, 7, 2, 2], 0, 2, []))
+def find_the_length_of_string(s):
+    if s == "":
+        return 0
+    else:
+        return 1 + find_the_length_of_string(s[1:])
+print(find_the_length_of_string("aa123"))
