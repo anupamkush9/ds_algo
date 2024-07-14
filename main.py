e88571760a0d9196e0fff4a771e97d83ca926c9d
Anupamkush9 <anupam.kush9@gmail.com>
2024-07-14 17:20:34 +0530

    element_indexes_of_target_element_done_by_recursion

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-def remove_duplicate(s, index):
-    if index == len(s)-1:
-        return s[index]
-    if s[index] not in s[index+1:]: 
-        return s[index] + remove_duplicate(s, index +1)
-    return remove_duplicate(s, index +1)
+# Question1:
+#     For a given integer array of sizeN.You have to find all the occurrences (indices) of a given element (Key)
+#     and print them. Use a recursive function to solve this problem.
+#     Sample Input: arr[ ] = {3, 2, 4, 5, 6, 2, 7, 2, 2}, key = 2
+#     Sample Output: 1 5 7 8
 
-print(remove_duplicate("anupam", 0))
+
+def element_indexes(l, indexes, target, results):
+    if indexes == len(l) -1:
+        if l[indexes] == target:
+            results.append(indexes)
+            return results
+    if indexes < len(l)-1:
+        if l[indexes] == target:
+            results.append(indexes) 
+            idx = indexes+1
+            return element_indexes(l, idx, target, results)
+        idx = indexes+1
+        return element_indexes(l, idx, target, results)
+
+print(element_indexes([3, 2, 4, 5, 6, 2, 7, 2, 2], 0, 2, []))
