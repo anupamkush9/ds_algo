bfc8da45f8957fd34a7d44e7e68f5720f5fefd61
Anupamkush9 <anupam.kush9@gmail.com>
2024-07-14 11:24:35 +0530

    remove duplicate from string done

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
+def remove_duplicate(s, index):
+    if index == len(s)-1:
+        return s[index]
+    if s[index] not in s[index+1:]: 
+        return s[index] + remove_duplicate(s, index +1)
+    return remove_duplicate(s, index +1)
 
-
-
-l = [100, 200, 300, 400, 5880, 3, 300, 88]
-
-def last_occurance_of_number(index_, list_, target):
-    if list_[index_] == target:
-        return index_
-    else:
-        if index_ > 0:
-            index_ -= 1
-            return last_occurance_of_number(index_,list_, target)
-        else:
-            return None
-
-print(last_occurance_of_number(index_ = len(l)-1, list_=l, target=300))
+print(remove_duplicate("anupam", 0))
