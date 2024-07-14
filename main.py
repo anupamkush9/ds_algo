ce6ae8efd7e3a4178ab872fade512d9bc0682cab
Anupamkush9 <anupam.kush9@gmail.com>
2024-07-14 23:43:31 +0530

    merge sort implemented

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
-# Question1:
-# Find the length of string
+def merge_sort(arr):
+    if len(arr)<=1:
+        return
+    mid = len(arr)//2
+    left = arr[:mid]
+    right = arr[mid:]
+    merge_sort(left)
+    merge_sort(right)
+    merge(left, right, arr)
 
-def find_the_length_of_string(s):
-    if s == "":
-        return 0
-    else:
-        return 1 + find_the_length_of_string(s[1:])
-print(find_the_length_of_string("aa123"))
+def merge(left, right, arr):
+    left_len = len(left)
+    right_len = len(right)
+    i = j = k = 0
+    
+    while i < left_len and j < right_len:
+        if left[i] <= right[j]:
+            arr[k] = left[i]
+            i += 1
+        else:
+            arr[k] = right[j]
+            j += 1
+        k += 1
+    
+    while i < left_len:
+        arr[k] = left[i]
+        k += 1
+        i += 1
+    while j < right_len:
+        arr[k] = right[j]
+        k += 1
+        j += 1
+
+l = [10,40,55,0,5]        
+merge_sort(l)
+print(l)
+
