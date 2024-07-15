321abaed42c10a253eb26bf80baea300d11fc82e
Anupamkush9 <anupam.kush9@gmail.com>
2024-07-15 18:17:18 +0530

    pairs in array implemented

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-def reverse_array(arr):
-    start = 0
-    end = len(arr) - 1
-    while(start < end):
-        arr[start], arr[end] = arr[end], arr[start]
-        start += 1
-        end -= 1
-    return arr
-arr = [0, 5, 12, 77, 88, 55]
-print("Before sorting array : ", arr)
-reverse_array(arr)
-print("After sorting array : ", arr)
+def pairs_in_array(arr):
+    pairs = []
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            pairs.append(str(f'{arr[i]}{arr[j]}'))
+    print(pairs)
+arr = [0, 5, 12, 8]
+pairs_in_array(arr)
