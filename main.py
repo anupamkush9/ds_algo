e67d896c19723f8bbb1467bfb50399014cd395d0
Anupamkush9 <anupam.kush9@gmail.com>
2024-07-15 13:38:37 +0530

    linear_search_in_array

--- main.py
+++ main.py
@@ -1,38 +1,10 @@
-def merge_sort(arr):
-    if len(arr)<=1:
-        return
-    mid = len(arr)//2
-    left = arr[:mid]
-    right = arr[mid:]
-    merge_sort(left)
-    merge_sort(right)
-    merge(left, right, arr)
+def linear_search(a, target):
+    for index, element in enumerate(a):
+        if element == target:
+            return index
+    else:
+        return -1
 
-def merge(left, right, arr):
-    left_len = len(left)
-    right_len = len(right)
-    i = j = k = 0
-    
-    while i < left_len and j < right_len:
-        if left[i] <= right[j]:
-            arr[k] = left[i]
-            i += 1
-        else:
-            arr[k] = right[j]
-            j += 1
-        k += 1
-    
-    while i < left_len:
-        arr[k] = left[i]
-        k += 1
-        i += 1
-    while j < right_len:
-        arr[k] = right[j]
-        k += 1
-        j += 1
-
-l = [10,40,55,0,5]        
-merge_sort(l)
-print(l)
-
-# ref : https://www.youtube.com/watch?v=nCNfu_zNhyI&t=353s
+a = [10,5,55,2,54]
+target = 54
+print(linear_search(a, target))
