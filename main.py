db278a879ad4354a38aeaabc6ad829304cc347c4
Anupamkush9 <anupam.kush9@gmail.com>
2024-07-16 17:22:09 +0530

    Added time complexity for binary search

--- main.py
+++ main.py
@@ -12,3 +12,7 @@ def binary_search(arr, target):
     return -1
 
 print(binary_search([0, 5, 12, 77, 88], -4))
+
+
+# ref for time complexity
+# https://stackoverflow.com/questions/8185079/how-to-calculate-binary-search-complexity
