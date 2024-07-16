a02aa3c73cdfd1776b28c80a2023f4a030e6757a
Anupamkush9 <anupam.kush9@gmail.com>
2024-07-16 20:28:04 +0530

    container with most water by 2 pointer approach implemented

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-def fibonacci(a, b, n):
-    if n==1:
-        return a + b
-    print(a + b)
-    return fibonacci(b, a+b, n-1)
+def container_with_most_water(arr):
+    max_contained_water = 0
+    left = 0
+    right = len(arr)-1
+    while(left < right):
+        height = min(arr[left], arr[right])
+        width = right - left
+        current_water = height * width
+        max_contained_water = max(max_contained_water, current_water)
+        if arr[left] < arr[right]:
+            left += 1
+        else:
+            right -= 1
+    return max_contained_water
 
-print(0)
-print(1)
-print(fibonacci(0,1,5))
+# l = [1, 5, 4, 3]
+l = [3, 1, 2, 4, 5]
+print(container_with_most_water(l))
+
+
+# ref
+# https://www.geeksforgeeks.org/container-with-most-water/
