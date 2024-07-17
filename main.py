4201cec4e5f0efb26719f95b1ec5fe20998fb68a
Anupamkush9 <anupam.kush9@gmail.com>
2024-07-17 08:57:52 +0530

    added utility method for linked list

--- main.py
+++ main.py
@@ -11,7 +11,18 @@ class LinkedList:
         new_node = Node(data, next)
         new_node.next = self.head
         self.head = new_node
+
+    def remove_first_element(self):
+        self.head = self.head.next
         
+    def remove_last_element(self):
+        current = self.head
+        prev = None
+        while current.next:
+            prev = current
+            current = current.next
+        prev.next = None
+
     def length_of_linked_list(self):
         count = 0
         current = self.head
@@ -43,7 +54,18 @@ class LinkedList:
             while current.next:
                 current = current.next
             current.next = new_node  
-        
+    
+    def search_element(self, target):
+        current = self.head
+        index = -1
+        while current.data:
+            index += 1  
+            if current.data == target:
+                return index
+            current = current.next
+        return index
+            
+            
     def printll(self):
         if self.head is None:
             print("Linked List is empty")
@@ -62,6 +84,9 @@ ll.insert(30)
 ll.insert(40)
 ll.insert(50)
 ll.inset_at_specific_index(1, 60)
+ll.remove_first_element()
+ll.remove_last_element()
+print(f"element is available at {ll.search_element(30)} index")
 ll.printll()
 print(f"lenght of linked_list is : ",ll.length_of_linked_list())
 
