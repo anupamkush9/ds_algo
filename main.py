01a2eb0d6408f4eecf0a90e1d46f2227d72a353a
Anupamkush9 <anupam.kush9@gmail.com>
2024-07-18 02:01:35 +0530

    duplicate pair parendthesis implemented

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-def fibonacci(a, b, n):
-    if n==1:
-        return a + b
-    print(a + b)
-    return fibonacci(b, a+b, n-1)
+def findDuplicateparenthesis(brackets):
+    stack = []
+    for bracket in brackets:
+        if bracket != ")":
+            stack.append(bracket)
+        elif bracket == ")":
+            count = 0
+            last_item = stack.pop()
+            while '(' != last_item:
+                count += 1
+            if count < 1:
+                return True
+    return False
+   
+brackets = "((a+b)+((c+d)))"
+brackets = "(a+b)"
+brackets = "((a+b+(c+d)))"
+print(findDuplicateparenthesis(brackets))
 
-print(0)
-print(1)
-print(fibonacci(0,1,5))
+# question ref : https://www.geeksforgeeks.org/problems/expression-contains-redundant-bracket-or-not/1
+# implementation dry and run ref : https://www.prepbytes.com/blog/stacks/expression-contains-redundant-bracket-or-not/
