ec18ea8f66dbd812d85e8e41ba3b782b42d92fc6
Anupamkush9 <anupam.kush9@gmail.com>
2024-07-18 16:03:10 +0530

    acitivity selection by greedy algorithm

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
-def findDuplicateparenthesis(brackets):
-    stack = []
-    for bracket in brackets:
-        if bracket != ")":
-            stack.append(bracket)
-        elif bracket == ")":
-            count = 0
-            last_item = stack.pop()
-            while '(' != last_item:
-                count += 1
-            if count < 1:
-                return True
-    return False
-   
-brackets = "((a+b)+((c+d)))"
-brackets = "(a+b)"
-brackets = "((a+b+(c+d)))"
-print(findDuplicateparenthesis(brackets))
+# Custom function
+def activitySelection(start, end):
+    end.sort()
+    activity_count = 0
+    acitvities = []
+    
+    acitvities.append(0)
+    last_activity_end_time = end[0]
+    activity_count = 1
+    for i in range(1,len(end)):
+        if start[i] > last_activity_end_time:
+            acitvities.append(i)
+            last_activity_end_time = end[i]
+            activity_count += 1
+    return activity_count
+            
 
-# question ref : https://www.geeksforgeeks.org/problems/expression-contains-redundant-bracket-or-not/1
-# implementation dry and run ref : https://www.prepbytes.com/blog/stacks/expression-contains-redundant-bracket-or-not/
+# start = [1, 3, 2, 5]
+# end = [2, 4, 3, 6]
+start = [2, 1]
+end = [2, 2]
+
+print("************")
+print(activitySelection(start, end))
+print("************")
+
+# Ref : https://www.geeksforgeeks.org/problems/activity-selection-1587115620/1
