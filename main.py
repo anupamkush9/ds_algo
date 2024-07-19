8f0901a99a76ad04fd232e61df2a1db45d7baac0
Anupamkush9 <anupam.kush9@gmail.com>
2024-07-19 23:02:06 +0530

    next greater element implemented by stack

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-def fibonacci(a, b, n):
-    if n==1:
-        return a + b
-    print(a + b)
-    return fibonacci(b, a+b, n-1)
+def next_greater_element(l):
+    list_with_next_greater_element = [-1] * len(l)
+    stack = []
 
-print(0)
-print(1)
-print(fibonacci(0,1,5))
+    for i in range(len(l) - 1, -1, -1):
+        while stack and stack[-1] <= l[i]:
+            stack.pop()
+        if stack:
+            list_with_next_greater_element[i] = stack[-1]
+        stack.append(l[i])
+
+    return list_with_next_greater_element
+
+# Example usage
+l = [1, 3, 2, 4]
+print("l:", l)
+print("Next Greater Elements:", next_greater_element(l))
+
+l = [10, 20, 30, 50]
+print("l:", l)
+print("Next Greater Elements:", next_greater_element(l))
+
+# ref : https://www.geeksforgeeks.org/problems/next-larger-element-1587115620/1
