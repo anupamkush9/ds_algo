4b04357984b70ba0ce22c4beec5fa61b585030c3
Anupamkush9 <anupam.kush9@gmail.com>
2024-07-20 18:05:44 +0530

    reverse_a_queue_implementation

--- main.py
+++ main.py
@@ -1,36 +1,9 @@
-from queue import Queue
-class Stack:
-    def __init__(self):
-        self.q = Queue()
 
-    def push(self, x):
-        s = self.q.qsize()
-        self.q.put(x)
-        for i in range(s):
-            self.q.put(self.q.get())
+def reverse_queue(l):
+    reversed_queue = []
+    for ele in l[::-1]:
+        reversed_queue.append(ele)
+    return reversed_queue
 
-    def pop(self):
-        n = self.q.get()
-        return n
-
-    def top(self):
-        return self.q.queue[0]
-
-    def size(self):
-        return self.q.qsize()
-
-if __name__ == "__main__":
-    s = Stack()
-    s.push(3)
-    s.push(2)
-    s.push(4)
-    s.push(1)
-    print("Top of the stack: ", s.top())
-    print("Size of the stack before removing element: ", s.size())
-    print("The deleted element is: ", s.pop())
-    print("Top of the stack after removing element: ", s.top())
-    print("Size of the stack after removing element: ", s.size())
-    
-    
-# ref : https://takeuforward.org/data-structure/implement-stack-using-single-queue/   
- 
+l = [1,2,3,4]
+print(reverse_queue(l))
