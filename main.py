96b43d5594c32d93501f06488cb6a3ba41222271
Anupamkush9 <anupam.kush9@gmail.com>
2024-07-20 15:29:38 +0530

    stack implementation using queue

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
-def fibonacci(a, b, n):
-    if n==1:
-        return a + b
-    print(a + b)
-    return fibonacci(b, a+b, n-1)
+from queue import Queue
+class Stack:
+    def __init__(self):
+        self.q = Queue()
 
-print(0)
-print(1)
-print(fibonacci(0,1,5))
+    def push(self, x):
+        s = self.q.qsize()
+        self.q.put(x)
+        for i in range(s):
+            self.q.put(self.q.get())
+
+    def pop(self):
+        n = self.q.get()
+        return n
+
+    def top(self):
+        return self.q.queue[0]
+
+    def size(self):
+        return self.q.qsize()
+
+if __name__ == "__main__":
+    s = Stack()
+    s.push(3)
+    s.push(2)
+    s.push(4)
+    s.push(1)
+    print("Top of the stack: ", s.top())
+    print("Size of the stack before removing element: ", s.size())
+    print("The deleted element is: ", s.pop())
+    print("Top of the stack after removing element: ", s.top())
+    print("Size of the stack after removing element: ", s.size())
+    
+    
+# ref : https://takeuforward.org/data-structure/implement-stack-using-single-queue/   
+ 
