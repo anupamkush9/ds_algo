c3bc9337f3b1d8d828b7617ce5f1a04d298b0373
Anupamkush9 <anupam.kush9@gmail.com>
2024-07-22 21:33:49 +0530

    max_consecutive_characters

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
-# Input: arr[] = {1, 1, 2}
-# Output: Yes
-# Explanation: Removing an occurrence of 1 modifies the array to {1, 2}, which is strictly increasing array.
+def maxConsecutivecharacters(l):
+    max_consicutive = 1
+    max_occurance_char = l[0]
+    current_consicutive = 0
+    last_char = l[0]
+    for ch in l[1:]:
+        if ch == last_char:
+            current_consicutive += 1
+            if current_consicutive > max_consicutive:
+                max_consicutive = max_consicutive + 1
+                max_occurance_char = ch
+        else:
+            current_consicutive = 1
+        last_char = ch
+    return max_consicutive, max_occurance_char            
 
-# Input: arr[] = {2, 2, 3, 4, 5, 5} 
-# Output: No
+    
+    
 
-def strictlyIncreasingArray(l):
-    non_strictly_increasing_element = 0
-    for i in range(len(l)-1):
-        if l[i] >= l[i+1]:
-            non_strictly_increasing_element += 1
-    return non_strictly_increasing_element == 1
-            
+l = [1,2,3,3,2,1,2]
+l = "geeeeekkkkkk"
+print(maxConsecutivecharacters(l))
 
-arr = [1, 1, 2]
-print("strictlyIncreasingArray===",strictlyIncreasingArray(arr))    
 
-# Ref : https://www.geeksforgeeks.org/check-whether-an-array-can-be-made-strictly-increasing-by-removing-at-most-one-element/
+# Ref : https://www.geeksforgeeks.org/maximum-consecutive-repeating-character-string/
