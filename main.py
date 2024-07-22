379c1ad0036454a5932907c722e9efcf74ec6026
Anupamkush9 <anupam.kush9@gmail.com>
2024-07-22 20:53:27 +0530

    flatten the list in python

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-def fibonacci(a, b, n):
-    if n==1:
-        return a + b
-    print(a + b)
-    return fibonacci(b, a+b, n-1)
+"""
+a program for Flattening an array
+INPUT:  [1 , 2 , [3, [4,5]]];
+OUTPUT:  [1,2,3,4,5]
 
-print(0)
-print(1)
-print(fibonacci(0,1,5))
+INPUT:   [[1 , 2], [3 , 4], [5 , 6]];
+OUTPUT:  [1,2,3,4,5,6]
+"""
+
+result = []
+
+def get_elements(list_):
+    for i in list_:
+        if type(i) != list:
+            result.append(i)
+        else:
+            get_elements(i)
+    return result
+
+list_ = [1, 2, [3, [4, 5]]]
+
+print(get_elements(list_))
