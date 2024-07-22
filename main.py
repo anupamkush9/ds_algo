aafa71cbc68c2f4d145a50aba334c0c6f8f88316
Anupamkush9 <anupam.kush9@gmail.com>
2024-07-23 03:58:35 +0530

    sorting_list_of_tuple_on_the_basis_of_2nd_argument

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-def fibonacci(a, b, n):
-    if n==1:
-        return a + b
-    print(a + b)
-    return fibonacci(b, a+b, n-1)
+# array_of_tuples = [(3, 'c'), (1, 'a'), (2, 'b')]
+# sorted_array = sorted(array_of_tuples, key=lambda x: x[1])
 
-print(0)
-print(1)
-print(fibonacci(0,1,5))
+# print(sorted_array)  # Output: [(1, 'a'), (2, 'b'), (3, 'c')]
+
+
+array_of_tuples = [(3, 'c'), (1, 'a'), (2, 'b')]
+sorted_array = sorted(array_of_tuples, key=lambda x: x[1], reverse=True)
+
+print(sorted_array)  # Output: [(3, 'c'), (2, 'b'), (1, 'a')]
