b060ca04602edfe6f7f6e2267aea554a92d5f4c7
Anupamkush9 <anupam.kush9@gmail.com>
2024-07-28 22:35:56 +0530

    top k element in array

--- main.py
+++ main.py
@@ -1,32 +1,14 @@
-def get_missing_number_by_hashing(l):
-    length_Of_arr = len(l) + 2
-    hash_arr = [0]*length_Of_arr
-    for i in l:
-        hash_arr[i] = 1
-    for i in range(1, len(hash_arr)):
-        if hash_arr[i] == 0:
-            return i
+from collections import Counter
 
-def get_missing_number_by_sum_of_n_natural_number(l):
-    max_element = max(l)
-    sum_of_n_natural_number = (max_element*(max_element+1))//2
-    sum_of_list = sum(l)
-    return sum_of_n_natural_number - sum_of_list
+def top_k_frequent_elements(nums, k):
+    counter = Counter(nums)
+    sorted_element_by_frequency = sorted(counter.items(), key=lambda x: (-x[1], -x[0]))
+    return [ele for ele,freq in sorted_element_by_frequency[:k]]
 
-def get_missing_number_by_xor_operator(l):
-    max_element = max(l)+1
-    ans = 0
-    for i in range(max_element):
-        ans ^= i
-    for ele in l:
-        ans ^= ele
-    return ans
-
-l = [1, 2, 4, 5]
-# print("ans::::", get_missing_number_by_hashing(l))
-# print("ans::::", get_missing_number_by_sum_of_n_natural_number(l))
-print("ans::::", get_missing_number_by_xor_operator(l))
+N = 8
+nums = [1, 1, 2, 2, 3, 3, 3, 4]
+k = 2
+print(top_k_frequent_elements(nums, k))
 
 
-# Question Ref :
-# https://takeuforward.org/arrays/find-the-missing-number-in-an-array/
+# Ref : https://www.geeksforgeeks.org/problems/top-k-frequent-elements-in-array/1
