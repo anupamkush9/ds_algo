90ec383b64a9a15f7ecb590fcdbef2ce023a1d4a
Anupamkush9 <anupam.kush9@gmail.com>
2024-07-29 21:29:09 +0530

    maximum_of_all_subarray_by_sliding_window

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-def fist_negative_number_of_every_window(A, k):
-    """
-        By brute force approach implementation.
-    """
+# largest subarray of sum k by sliding window
+
+def maximum_of_all_subarray(array, k):
     ans = []
-    for i in range(len(A)-k+1):
-        min_element_in_window = min(A[i:i+k])
-        ans.append(min_element_in_window) if min_element_in_window < 0 else ans.append(0)
+    for i in range(len(array)-k+1):
+        ans.append(max(array[:i+k]))
     return ans
+        
+
+k = 3
+arr = [1, 2, 3, 1, 4, 5, 2, 3, 6]
+# Output: 
+# 3 3 4 5 5 5 6 
 
 
-A = [-8, 2, 3, -6, 10]
-K = 2
-print("==>>",fist_negative_number_of_every_window(A, K))
-# Output : 
-# -8 0 -6 -6
+# k = 4
+# arr = [8, 5, 10, 7, 9, 4, 15, 12, 90, 13]
+# Output: 
+# 10 10 10 15 15 90 90
+print("output ::: ",*maximum_of_all_subarray(arr, k))
 
 
-# https://www.geeksforgeeks.org/problems/first-negative-integer-in-every-window-of-size-k3345/1
+# Question Ref:
+# https://takeuforward.org/data-structure/sliding-window-maximum/f
+# https://www.geeksforgeeks.org/problems/maximum-of-all-subarrays-of-size-k3101/1
+
