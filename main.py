f5c11996806871f83457897ae10e22a498cb398d
Anupamkush9 <anupam.kush9@gmail.com>
2024-07-29 21:15:00 +0530

    negative_integer_in_every_window_of_size_k_by_sliding_window

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
-# count occurances of anagram by sliding window
-
-def get_anagram_string_count(s, anagram_string):
-    count = 0
-    anagram_string_sorted = ''.join(sorted(anagram_string))
-    for i in range(len(s)-len(anagram_string)+1):
-        window_str_sorted = ''.join(sorted(s[i:i+len(anagram_string)]))
-        if window_str_sorted == anagram_string_sorted:
-            count += 1
-    return count
-
-s = "forxxorfxdofr"
-anagram_string = "for"
-
-# s = "aabaabaa"
-# anagram_string = "aaba"
-print("output ::: ",get_anagram_string_count(s, anagram_string))
+def fist_negative_number_of_every_window(A, k):
+    """
+        By brute force approach implementation.
+    """
+    ans = []
+    for i in range(len(A)-k+1):
+        min_element_in_window = min(A[i:i+k])
+        ans.append(min_element_in_window) if min_element_in_window < 0 else ans.append(0)
+    return ans
 
 
-# Question Ref:
-# https://www.geeksforgeeks.org/count-occurrences-of-anagrams/
+A = [-8, 2, 3, -6, 10]
+K = 2
+print("==>>",fist_negative_number_of_every_window(A, K))
+# Output : 
+# -8 0 -6 -6
+
+
+# https://www.geeksforgeeks.org/problems/first-negative-integer-in-every-window-of-size-k3345/1
