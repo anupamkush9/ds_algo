89c2c7488f37bdb0dfbfa1af5577a83a3cc04e3e
Anupam Kushwaha <67794478+anupamkush9@users.noreply.github.com>
2025-04-14 19:01:21 +0530

    Added manual way of finding top_k_frequent_elements

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from collections import Counter
 
-def top_k_frequent_elements(nums, k):
-    counter = Counter(nums)
-    sorted_element_by_frequency = sorted(counter.items(), key=lambda x: (-x[1], -x[0]))
-    return [ele for ele,freq in sorted_element_by_frequency[:k]]
+# def top_k_frequent_elements(nums, k):
+#     counter = Counter(nums)
+#     sorted_element_by_frequency = sorted(counter.items(), key=lambda x: (-x[1], -x[0]))
+#     return [ele for ele,freq in sorted_element_by_frequency[:k]]
+
+def top_k_frequent_elements(nums,k):
+    count_dict = {}
+    for item in nums:
+        if item not in count_dict.keys():
+            count_dict[item] = 1
+        else:
+            count_dict[item] = count_dict[item] + 1
+    sorted_dict = dict(sorted(count_dict.items(), key=lambda item: item[1], reverse=True))
+    return list(sorted_dict.keys())[:k]
 
 N = 8
 nums = [1, 1, 2, 2, 3, 3, 3, 4]
