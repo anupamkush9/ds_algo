c8212029b4d8d65e73663aae3edb07aee560cc5e
Anupamkush9 <anupam.kush9@gmail.com>
2026-06-22 18:03:30 +0530

    implemented find all possible pairs

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
-def fibonacci(a, b, n):
-    if n==1:
-        return a + b
-    print(a + b)
-    return fibonacci(b, a+b, n-1)
+"""
+Input : lst =[1, 5, 3, 7, 9]
+        K = 12
+Output : [(5, 7), (3, 9)]
 
-print(0)
-print(1)
-print(fibonacci(0,1,5))
+
+int_array = [2,3,5,7,8,9]
+Output : [(2, 8), (3, 7)]
+
+Ref:
+https://www.geeksforgeeks.org/python/python-program-to-find-all-possible-pairs-with-given-sum/
+
+"""
+
+
+def calculate_sum_of_array(int_array, target_sum):
+    left = 0
+    right = len(int_array)-1
+    correct_pairs = []
+    while left <= right:
+        if ((int_array[left]+int_array[right]) == target_sum):
+            if (left != right):
+                correct_pairs.append((int_array[left], int_array[right]))
+            left = left+1
+            right = right-1
+        elif (int_array[left]+int_array[right]) > target_sum:
+            right = right-1
+        else:
+            left = left + 1
+    return correct_pairs
+
+int_array = [2,3,5,7,8,9]
+target = int(input("Please enter target value\n"))
+print("sum of pairs are : ", calculate_sum_of_array(int_array, target))
+
