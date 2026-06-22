814f62ba859bcaa2e5cc65486eb5445644425469
Anupam Kushwaha <anupamkush9@gmail.com>
2026-06-22 18:32:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,27 @@
 """
-Input: s = "geeekk"
-Output: e
-Explanation: character e comes 3 times consecutively which is maximum.
 
-Input: s = "aaaabbcbbb"
-Output: a
-Explanation: character a comes 4 times consecutively which is maximum.
+input_str = "aaabbbbcccccaaa"
+c = 5
 """
-
-def maxConsecutivecharacters(l):
-    max_consicutive = 1
-    max_occurance_char = l[0]
-    current_consicutive = 0
-    last_char = l[0]
-    for ch in l[1:]:
-        if ch == last_char:
-            current_consicutive += 1
-            if current_consicutive > max_consicutive:
-                max_consicutive = max_consicutive + 1
-                max_occurance_char = ch
+def get_max_occurance_of_consecutive_character(input_str):
+    max_count = 1
+    max_character = input_str[0]
+    current_count = 1
+    prev_char = input_str[0]
+    
+    for current_char in input_str[1:]:
+        if current_char == prev_char:
+            current_count = current_count + 1
+            if max_count < current_count:
+                max_count = current_count
+                max_character = current_char
         else:
-            current_consicutive = 1
-        last_char = ch
-    return max_consicutive, max_occurance_char            
-
+            prev_char = current_char
+            current_count = 1
+    return max_character, max_count
+        
     
-    
-
-l = [1,2,3,3,2,1,2]
-l = "geeeeekkkkkk"
-print(maxConsecutivecharacters(l))
-
+input_str = "aaabbbbcccccaaa"
+print(get_max_occurance_of_consecutive_character(input_str))
 
 # Ref : https://www.geeksforgeeks.org/maximum-consecutive-repeating-character-string/
