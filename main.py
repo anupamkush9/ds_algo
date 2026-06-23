1f7841a281256f9e3f5fd72ab88e40f61ceff21a
Anupamkush9 <anupam.kush9@gmail.com>
2026-06-23 22:34:12 +0530

    Find the missing number in an array implemented

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 """
-Input: s1 = “geeks”  s2 = “kseeg”
-Output: true
-Explanation: Both the string have same characters with same frequency. So, they are anagrams.
 
-Input: s1 = "allergy", s2 = "allergyy"
-Output: false
-Explanation: Although the characters are mostly the same, s2 contains an extra 'y' character. Since the frequency of characters differs, the strings are not anagrams.
+Example 1:
+Input Format: N = 5, array[] = {1,2,4,5}
+Result: 3
+Explanation: In the given array, number 3 is missing. So, 3 is the answer.
 
-Input: s1 = "listen", s2 = "lists"
-Output: false
-Explanation: The characters in the two strings are not the same — some are missing or extra. So, they are not anagrams.     
+
+Example 2:
+Input Format: N = 3, array[] = {1,3}
+Result: 2
+Explanation: In the given array, number 2 is missing. So, 2 is the answer.
 
 Ref :
 https://takeuforward.org/data-structure/check-if-two-strings-are-anagrams-of-each-other
@@ -17,24 +17,24 @@ https://www.geeksforgeeks.org/dsa/check-whether-two-strings-are-anagram-of-each-
 
 """
 
-def areAnagrams(s1, s2):
-    
-    if len(s1) != len(s2):
-        return False
-    
-    # Sort both strings
-    s1 = sorted(s1)
-    s2 = sorted(s2)
 
-    # Compare sorted strings
-    return s1 == s2
+# def find_missing_number(nums):
+#     for i in range(len(nums)-1):
+#         if nums[i+1] - nums[i] != 1:
+#             return nums[i] + 1
 
-if __name__ == "__main__":
+# nums = [1,2,3,4,6,7,8,9,10,11]
+# print("missing number is : ",find_missing_number(nums))
     
-    s1 = "geeks"
-    s2 = "kseeg"
-    
-    if(areAnagrams(s1,s2)):
-        print("true")
-    else:
-        print("false")
+class Solution:
+    def missingNumber(self, a, N):
+        total_sum = (N * (N + 1)) // 2
+        actual_sum = sum(a)
+        return total_sum - actual_sum
+
+# Driver code
+a = [1, 2, 4, 5]
+N = 5
+obj = Solution()
+ans = obj.missingNumber(a, N)
+print("The missing number is:", ans)
