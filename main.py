94424c8b1d42f20a9816d8d514b4026eefa4a9dd
Anupamkush9 <anupam.kush9@gmail.com>
2026-06-25 22:22:02 +0530

    two sum problem

--- main.py
+++ main.py
@@ -1,40 +1,68 @@
 """
 
-Example 1:
-Input Format: N = 5, array[] = {1,2,4,5}
-Result: 3
-Explanation: In the given array, number 3 is missing. So, 3 is the answer.
+Input: N = 5, arr[] = {2,6,5,8,11}, target = 14
+Output : YES
+Explanation: arr[1] + arr[3] = 14. So, the answer is “YES” for first variant for second variant output will be : [1,3].
 
+Input: N = 5, arr[] = {2,6,5,8,11}, target = 15
+Output : NO.
+Explanation: There exist no such two numbers whose sum is equal to the target. 
 
-Example 2:
-Input Format: N = 3, array[] = {1,3}
-Result: 2
-Explanation: In the given array, number 2 is missing. So, 2 is the answer.
-
-Ref :
-https://takeuforward.org/data-structure/check-if-two-strings-are-anagrams-of-each-other
-https://www.geeksforgeeks.org/dsa/check-whether-two-strings-are-anagram-of-each-other/
-
+Ref : https://takeuforward.org/data-structure/two-sum-check-if-a-pair-with-given-sum-exists-in-array
+https://interviewing.io/questions/two-sum
 """
 
 
-# def find_missing_number(nums):
-#     for i in range(len(nums)-1):
-#         if nums[i+1] - nums[i] != 1:
-#             return nums[i] + 1
+class SolutionBruteForce:
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
 
-# nums = [1,2,3,4,6,7,8,9,10,11]
-# print("missing number is : ",find_missing_number(nums))
-    
-class Solution:
-    def missingNumber(self, a, N):
-        total_sum = (N * (N + 1)) // 2
-        actual_sum = sum(a)
-        return total_sum - actual_sum
 
-# Driver code
-a = [1, 2, 4, 5]
-N = 5
-obj = Solution()
-ans = obj.missingNumber(a, N)
-print("The missing number is:", ans)
+class SolutionTwoPointers:
+    def twoSum(self, nums, target):
+        arr = [(nums[i], i) for i in range(len(nums))]
+        arr.sort()
+
+        left, right = 0, len(arr) - 1
+
+        while left < right:
+            curr_sum = arr[left][0] + arr[right][0]
+
+            if curr_sum == target:
+                return [arr[left][1], arr[right][1]]
+            elif curr_sum < target:
+                left += 1
+            else:
+                right -= 1
+
+        return []
+
+
+class SolutionHashMap:
+    def twoSum(self, nums, target):
+        num_to_index = {}
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+
+            if complement in num_to_index:
+                return [num_to_index[complement], i]
+
+            num_to_index[nums[i]] = i
+
+        return []
+
+
+# ------------------------------------------------------------
+# Driver Code
+# ------------------------------------------------------------
+nums = [2, 7, 11, 15]
+target = 9
+
+print("Approach 1:", SolutionBruteForce().twoSum(nums, target))
+print("Approach 2:", SolutionTwoPointers().twoSum(nums, target))
+print("Approach 3:", SolutionHashMap().twoSum(nums, target))
