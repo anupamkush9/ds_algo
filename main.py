f097aaf9f0545b6b7faa85e0a051ed117bb9eed5
Anupamkush9 <anupam.kush9@gmail.com>
2026-06-28 12:25:35 +0530

    3-sum-triplet-problem-solved

--- main.py
+++ main.py
@@ -1,39 +1,48 @@
 """
-Input: prices[] = [7, 10, 1, 3, 6, 9, 2]
-Output: 8
-Explanation: Buy for price 1 and sell for price 9. 
+Input: arr[] = [1, 4, 45, 6, 10, 8], target = 13
+Output: true
+Explanation: The triplet [1, 4, 8] sums up to 13
 
-Input: prices[] = [7, 6, 4, 3, 1]
-Output: 0
-Explanation: Since the array is sorted in decreasing order, 0 profit can be made without making any transaction.
+Input: arr[] = [1, 2, 4, 3, 6, 7], target = 10 
+Output: true
+Explanation: The triplets [1, 3, 6] and [1, 2, 7] both sum to 10. 
 
-Input: prices[] = [1, 3, 6, 9, 11]
-Output: 10
-Explanation: Since the array is sorted in increasing order, we can make maximum profit by buying at price[0] and selling at price[n-1]
+Input: arr[] = [40, 20, 10, 3, 6, 7], sum = 24 
+Output: false
+Explanation:  No triplet in the array sums to 24.
 
 Ref:
-https://www.geeksforgeeks.org/dsa/best-time-to-buy-and-sell-stock/
-https://takeuforward.org/data-structure/stock-buy-and-sell
+https://www.geeksforgeeks.org/dsa/find-a-triplet-that-sum-to-a-given-value/
+
 """
 
+def hasTripletSum(arr, target):
+    n = len(arr)
+    arr.sort()
+    
+    # Fix the first element as arr[i]
+    for i in range(n - 2):
+        
+        # Initialize left and right pointers with 
+        # start and end of remaining subarray
+        l = i + 1
+        r = n - 1
+        
+        requiredSum = target - arr[i]
+        while l < r:
+            if arr[l] + arr[r] == requiredSum:
+                return True
+            if arr[l] + arr[r] < requiredSum:
+                l += 1
+            else:
+                r -= 1
+    
+    return False
 
-def maxProfit(prices):
-    minSoFar = prices[0]
-    max_profit = 0
-    for i in range(1, len(prices)):
-        if minSoFar > prices[i]:
-            minSoFar = prices[i]
-        else:
-            max_profit = max(max_profit, prices[i] - minSoFar)
-    return max_profit
-
-# Driver code
-if __name__ == '__main__':
-
-    prices = [7, 1, 5, 6, 4]
-    max_profit = maxProfit(prices)
-    print(max_profit)
-
-# ref: 
-# https://www.geeksforgeeks.org/best-time-to-buy-and-sell-stock/
-# best time to buy stock
+if __name__ == "__main__":
+    arr = [1, 4, 45, 6, 10, 8]
+    target = 13
+    if hasTripletSum(arr, target):
+        print("true")
+    else:
+        print("false")
