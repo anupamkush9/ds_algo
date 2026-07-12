de68bddfcb71e38262592c6cd070502ce8f99a90
Anupam Kushwaha <anupamkush9@gmail.com>
2026-07-12 18:16:17 +0530

    Add docstring for top_k_frequent_elements function
    Added docstring with examples for top_k_frequent_elements function.

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+"""
+Input: arr[] = [3, 1, 4, 4, 5, 2, 6, 1], k = 2
+Output: [4, 1]
+Explanation: Frequency of 4 is 2 and frequency of 1 is 2, these two have the maximum frequency and 4 is larger than 1.
+
+Input: arr[] = [7, 10, 11, 5, 2, 5, 5, 7, 11, 8, 9], k = 4
+Output: [5, 11, 7, 10]
+Explanation: Frequency of 5 is 3, frequency of 11 is 2, frequency of 7 is 2, frequency of 10 is 1.
+"""
+
 from collections import Counter
 
 # def top_k_frequent_elements(nums, k):
